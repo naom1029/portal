6d2877a01a2c039bf562ea524688e480528aeb23
naom971029 <bluesky.97102945@gmail.com>
2024-09-28 00:23:23 +0900

    chore:update tailwind setting

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,25 @@ module.exports = {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          light: '#647cff',
+          DEFAULT: '#535bf2',
+          dark: '#3b47cc',
+        },
+        secondary: {
+          light: '#ffdd57',
+          DEFAULT: '#ffd600',
+          dark: '#e6c200',
+        },
+      },
+      fontFamily: {
+        sans: ['Inter', 'system-ui', 'Avenir', 'Helvetica', 'Arial', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
